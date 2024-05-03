ec363a7c6319b383d09131d21cc19f82ea5e6a85
johanpq <johanp.queiroz@gmail.com>
2024-05-03 16:04:14 -0300

    feat: changed selector

--- script.js
+++ script.js
@@ -21,31 +21,41 @@ let zero = false;
 let isOp = false;
 let isComma = false;
 
+let countTeclas = 0;
+
 clean.addEventListener('click', () => {
     display.innerHTML = "0";
     zero = false;
+    countTeclas = 0;
 } );
 
 const print = document.addEventListener('click', (event) => {
     const p = document.createElement('p');
 
-    if(event.target.classList.contains('op2')) {
-        event.target = "";
-    } else if(event.target.classList.contains('tecla')) {
-            if(zero == false) {
-                zero = true
-                display.innerHTML = "";
-            }
-            
-            if(event.target.classList.contains('op') && isOp == false) {
-                isOp = true;
-                p.innerHTML= event.target.textContent;
-            } else if(!event.target.classList.contains('op')) {
-                isOp = false;
-                p.innerHTML= event.target.textContent;
-            }
-    
-            display.appendChild(p);
+    if(event.target.classList.contains('tecla')) {
+        if(zero == false) {
+            zero = true;
+            display.innerHTML = "";
         }
-    }
-)
+
+        if(event.target.classList.contains('number')) {
+            countTeclas++
+            p.innerHTML = event.target.textContent;
+        }
+
+        if(event.target.classList.contains('op') && isOp == false) {
+            if(countTeclas > 0) {
+                isOp = true;
+                p.innerHTML = event.target.textContent;
+            }
+
+        } else if(!event.target.classList.contains('op')) {
+            isOp = false;
+        }
+
+        console.log(countTeclas);
+        
+    } 
+
+    display.appendChild(p);
+ })
